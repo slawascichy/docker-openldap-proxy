da06b5c9e9be42d6caf56276aabea666baa5b254
Sławomir 'slawas' Cichy <slawas@slawas.pl>
2025-08-04 10:05:01 +0200

    Zmienił się numer bazy meta

--- service/add-mapping-of-attribute-names-AD-to-OpenLDAP.sh
+++ service/add-mapping-of-attribute-names-AD-to-OpenLDAP.sh
@@ -5,7 +5,7 @@ echo "* Adding mapping of attribute names from external AD to local OpenLDAP..."
 echo "**************************"
 # $1 - the name of the organizational unit under which the connected LDAP tree should appear
 
-export META_SUB=`ldapsearch -Y EXTERNAL -H ldapi:/// -b "olcDatabase={3}meta,cn=config" -s sub "(olcMetaSub=${1})" olcMetaSub | grep "olcMetaSub:" | awk '{print $2}'`
+export META_SUB=`ldapsearch -Y EXTERNAL -H ldapi:/// -b "olcDatabase={4}meta,cn=config" -s sub "(olcMetaSub=${1})" olcMetaSub | grep "olcMetaSub:" | awk '{print $2}'`
 LDIF_FILE=/tmp/add-all-dbmap-for-ad-proxy.ldif
 cat  /opt/init/06-add-all-dbmap-for-ad-proxy.ldif | envsubst > $LDIF_FILE
 ldapmodify -Y EXTERNAL -H ldapi:/// -f $LDIF_FILE
